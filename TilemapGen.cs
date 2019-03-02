b65519326fea0de4358cdfe4bbf83e79c09fee36
Omar Delarosa <thedelarosa@gmail.com>
2019-03-02 11:58:14 -0500

    added core barrier generator

--- TilemapGen.cs
+++ TilemapGen.cs
@@ -15,13 +15,24 @@ namespace TilemapGen
     {
         static void Main()
         {
-            Vector2Int size = new Vector2Int(16, 16);
-            Matrix m = new Matrix(size);
-            m.PrintMatrix();
+            Console.WriteLine("MAP GENERATOR");
 
+            ConsoleKeyInfo cki = CLI.Prompt();
+            while (cki.Key == ConsoleKey.Enter)
+            {
+                Vector2Int size = new Vector2Int(16, 16);
+                Matrix m = new Matrix(size);
+                m.PrintMatrix();
+                cki = CLI.Prompt();
+            }
+        }
+
+        static ConsoleKeyInfo Prompt()
+        {
+            ConsoleKeyInfo cki = Console.ReadKey();
             // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.WriteLine("Press ENTER to generate a map.  Anything else to quit."); ;
+            return cki;
         }
     }
 
@@ -35,16 +46,26 @@ namespace TilemapGen
             this.x = x;
             this.y = y;
         }
+
+        override public string ToString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
     }
 
     class Matrix
     {
         public const string PADDING = " ";
         List<List<Tile>> tilematrix;
+        List<Vector2Int> positions;
         private Vector2Int size;
+        Random r;
         public Matrix(Vector2Int size)
         {
+            r = new Random();
+            positions = new List<Vector2Int>();
             BuildEmptyMatrix(size);
+            FillMatrix();
         }
 
         void BuildEmptyMatrix(Vector2Int s)
@@ -59,10 +80,56 @@ namespace TilemapGen
                 for (int j = 0; j < y; j++)
                 {
                     row.Add(Tile.NullTile);
+                    Vector2Int pos = new Vector2Int(i, j);
+                    positions.Add(pos);
                 }
                 tilematrix.Add(row);
             }
         }
+
+        void FillMatrix()
+        {
+            foreach (Vector2Int pos in positions)
+            {
+                FillAtPosition(pos);
+            }
+        }
+
+        // TODO: generalize rules based on position better?
+        void FillAtPosition(Vector2Int pos)
+        {
+            if (IsBorder(pos) || IsRandomBarrier())
+            {
+                tilematrix[pos.x][pos.y] = Tile.Barrier;
+            }
+            else
+            {
+                tilematrix[pos.x][pos.y] = Tile.Space;
+            }
+        }
+
+        bool IsBorder(Vector2Int pos)
+        {
+            if (pos.x == 0 || pos.x == size.x - 1 || pos.y == 0 || pos.y == size.y - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool IsRandomBarrier()
+        {
+            int rInt = r.Next(0, 10);
+            if (rInt < 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         string TileToString(Tile t)
         {
             switch (t)
@@ -78,7 +145,7 @@ namespace TilemapGen
             }
         }
 
-        public string ToString()
+        public override string ToString()
         {
             int x = size.x;
             int y = size.y;
